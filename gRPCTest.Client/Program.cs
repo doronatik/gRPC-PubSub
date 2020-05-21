c8599b95ed50729e369d0b2ed6d7add730c756d0
Doron Atik <datik@controlrad.com>
2020-05-21 14:43:32 +0300

    Add project files.

---
+++ gRPCTest.Client/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Pubsub;
+
+namespace gRPCTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Environment.SetEnvironmentVariable("GRPC_TRACE", "tcp,channel,http,secure_endpoint");
+            //Environment.SetEnvironmentVariable("GRPC_VERBOSITY", "DEBUG");
+
+
+            var channel = new Channel("127.0.0.1:50052", ChannelCredentials.Insecure/*, new[] { new ChannelOption("grpc.keepalive_time_ms", 10 * 1000),
+                                                                                              new ChannelOption("grpc.keepalive_permit_without_calls", 1)}*/);
+            var subscriber = new Subscriber(new PubSub.PubSubClient(channel));
+            
+            
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    Console.WriteLine($"Status: {channel.State}");
+                    await Task.Delay(50);
+                }
+            });
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await channel.WaitForStateChangedAsync(ChannelState.Ready);
+                    Console.WriteLine("DISCONNECTION!!!!!!!!!!!!!!");
+                }
+            });
+
+
+            channel.ConnectAsync();
+            Subscribe(subscriber);
+
+            Console.WriteLine("Hit 'q' to unsubscribe, 'c' to reconnect");
+            char key;
+
+            while ((key = Console.ReadKey().KeyChar) != 'q')
+            {
+                switch (key)
+                {
+                    case 's':
+                        {
+                            Console.WriteLine(channel.State);
+                            break;
+                        }
+                    case 'c':
+                        {
+                            channel.ConnectAsync();
+                            break;
+                        }
+                    case 'r':
+                        {
+                            Subscribe(subscriber);
+                            break;
+                        }
+                    case 'q':
+                        {
+                            break;
+                        }
+                }
+            }
+
+            subscriber.Unsubscribe();
+
+            Console.WriteLine("Unsubscribed...");
+
+            Console.WriteLine("Hit key to exit...");
+            Console.ReadLine();
+            
+        }
+
+        private static void Subscribe(Subscriber subscriber)
+        {
+            Task.Run(async () =>
+            {
+                await subscriber.Subscribe();
+            }).GetAwaiter();
+
+        }
+
+        public class Subscriber
+        {
+            private readonly PubSub.PubSubClient _pubSubClient;
+            private Subscription _subscription;
+
+            public Subscriber(PubSub.PubSubClient pubSubClient)
+            {
+                _pubSubClient = pubSubClient;
+            } 
+
+            public async Task Subscribe()
+            {
+                _subscription = new Subscription() { Id = Guid.NewGuid().ToString() };
+                using (var call = _pubSubClient.Subscribe(_subscription))
+                {
+                    //Receive
+                    var responseReaderTask = Task.Run(async () =>
+                    {
+                        while (await call.ResponseStream.MoveNext())
+                        {
+                            Console.WriteLine("Event received: " + call.ResponseStream.Current);
+                        }
+                    });
+
+                    await responseReaderTask;
+                }
+            }
+
+            public void Unsubscribe()
+            {
+                _pubSubClient.Unsubscribe(_subscription);
+            }
+        }
+    }
+}
---
+++ gRPCTest.Server/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Grpc.Core;
+using Pubsub;
+
+namespace gRPCTest.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("GRPC_TRACE", "tcp,channel,http,secure_endpoint");
+            Environment.SetEnvironmentVariable("GRPC_VERBOSITY", "DEBUG");
+
+            //List<ChannelOption> channelOptions = new List<ChannelOption>();
+            //channelOptions.Add(new ChannelOption("grpc.http2.min_ping_interval_without_data_ms", 1000));
+            //channelOptions.Add(new ChannelOption("grpc.http2.max_pings_without_data", 0));
+            //channelOptions.Add(new ChannelOption("grpc.keepalive_permit_without_calls", 1));
+            //channelOptions.Add(new ChannelOption("grpc.keepalive_time_ms", 10 * 1000));
+
+            const int port = 50052;
+            var pubsubImp = new PubSubImpl();
+            Grpc.Core.Server server = new Grpc.Core.Server(/*channelOptions*/)
+            {
+                Services = { PubSub.BindService(pubsubImp) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure)}
+            };
+
+            server.Start();
+
+            Console.WriteLine("RouteGuide server listening on port " + port);
+            Console.WriteLine("Insert event. 'q' to quit.");
+            string input;
+            while ((input = Console.ReadLine()) != "q")
+            {
+                pubsubImp.Publish(input);
+            }
+
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+    public class PubSubImpl : PubSub.PubSubBase
+    {
+        private readonly BufferBlock<Event> _buffer = new BufferBlock<Event>();
+
+        private Dictionary<string, IServerStreamWriter<Event>> _subscriberWritersMap = new Dictionary<string, IServerStreamWriter<Event>>();
+
+        public override async Task Subscribe(Subscription subscription, IServerStreamWriter<Event> responseStream, ServerCallContext context)
+        {
+            _subscriberWritersMap[subscription.Id] = responseStream;
+
+            while (_subscriberWritersMap.ContainsKey(subscription.Id))
+            {
+                var ev = await _buffer.ReceiveAsync();
+                foreach (var serverStreamWriter in _subscriberWritersMap.Values)
+                {
+                    try
+                    {
+                        await serverStreamWriter.WriteAsync(ev);
+                    }
+                    catch (Exception e)
+                    {
+                        //_subscriberWritersMap.Remove(subscription.Id);
+                    }
+                }
+            }
+        }
+
+        public override Task<Unsubscription> Unsubscribe(Subscription request, ServerCallContext context = null)
+        {
+            _subscriberWritersMap.Remove(request.Id);
+            return Task.FromResult(new Unsubscription() { Id = request.Id });
+        }
+
+        public void Publish(string input)
+        {
+            //Only publish when there are subscribers
+            if (_subscriberWritersMap.Count > 0)
+            {
+                _buffer.Post(new Event() { Value = input });
+            }
+        }
+    }
+}
